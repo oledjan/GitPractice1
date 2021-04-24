96e2ec763a2adbf46f8b6f4b51da46cb5fae1f3f
Oleg_Dziabenko <dziabenkoo@gmail.com>
2021-04-24 15:52:54 -0500

    added more

--- src/GitDay1/HelloWorld.java
+++ src/GitDay1/HelloWorld.java
@@ -3,5 +3,7 @@ package GitDay1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Adding GitHub");
+        System.out.println("Yay Stuffs are on GITHUB");
     }
 }
